2ba01a554286cbe814dfbd89580cea2cd20a7eec
ATP <atpdev24@gmail.com>
2022-10-15 23:41:16 +0100

    First commit.

---
+++ plus-minus/core.clj
@@ -0,0 +1,14 @@
+(defn plusMinus
+  [arr]
+  (let [{p :p
+         n :n
+         z :z} ((fn [arr] (reduce #(update %1 (cond (> %2 0) :p (< %2 0) :n (= %2 0) :z) inc) {:p 0 :n 0 :z 0} arr)) arr)] 
+    (println (format "%.6f" (float (/ p (+ p n z)))))
+    (println (format "%.6f" (float (/ n (+ p n z)))))
+    (println (format "%.6f" (float (/ z (+ p n z))))))
+  )
+
+(def n (Integer/parseInt (clojure.string/trim (read-line))))
+(def arr (vec (map #(Integer/parseInt %) (clojure.string/split (clojure.string/trim (read-line)) #" "))))
+
+(plusMinus arr)
